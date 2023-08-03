3b62feb2e045ad6b65defdedb5b9bc7cc4cb62f9
kimdaeyeub <kdy3915@naver.com>
2023-08-03 15:14:43 +0900

    HomeScreen

--- lib/features/home_screen.dart
+++ lib/features/home_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,80 +11,252 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.house,
+              color: Colors.black,
+              size: 27,
+            ),
+            label: "Home",
           ),
-          child: Column(
-            children: [
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              color: Colors.black,
+              size: 27,
+            ),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.run_circle,
+              size: 27,
+              color: Colors.black,
+            ),
+            label: "Challenge",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.settings,
+              size: 27,
+              color: Colors.black,
+            ),
+            label: "Settings",
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 25,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 15,
+                ),
+                const Text(
+                  "Logo",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 35,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(children: [
                     Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 20,
-                        vertical: 20,
-                      ),
+                      clipBehavior: Clip.hardEdge,
+                      height: 400,
+                      width: 300,
                       decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(20),
-                        boxShadow: const [],
                       ),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
+                      child: Column(
                         children: [
-                          CustomPaint(
-                            size: const Size(100, 100),
-                            painter: TimerPainter(),
-                          ),
-                          const SizedBox(
-                            width: 60,
-                          ),
-                          Column(
-                            children: [
-                              const Text(
-                                "코딩",
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Row(
-                                children: [
-                                  Text(
-                                    "하루 6시간",
-                                    style: TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w500,
-                                      color: Colors.grey.shade500,
-                                    ),
-                                  )
-                                ],
-                              )
-                            ],
+                          SizedBox(
+                            height: 250,
+                            width: 300,
+                            child: Image.asset(
+                              "assets/images/book-photo.jpg",
+                              fit: BoxFit.fitWidth,
+                            ),
                           )
                         ],
                       ),
                     ),
                     const SizedBox(
-                      width: 100,
+                      width: 20,
                     ),
                     Container(
-                      width: 400,
-                      height: 300,
-                      color: Colors.teal,
-                      child: const Text("data"),
+                      height: 400,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
                     ),
                     const SizedBox(
-                      width: 100,
+                      width: 20,
                     ),
-                  ],
+                    Container(
+                      height: 400,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ]),
                 ),
-              )
-            ],
+                const SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  "오늘 할 일",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey.shade600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                // 오늘 할 일 진행도
+                FractionallySizedBox(
+                  widthFactor: 1,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 25,
+                      vertical: 25,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(
+                        20,
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "코딩",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 15,
+                            ),
+                            CustomPaint(
+                              size: const Size(285, 10),
+                              painter: BarPainter(),
+                            )
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "독서",
+                              style: TextStyle(
+                                fontSize: 15,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 15,
+                            ),
+                            CustomPaint(
+                              size: const Size(285, 8),
+                              painter: BarPainterForBook(),
+                            )
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "운동",
+                              style: TextStyle(
+                                fontSize: 15,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 15,
+                            ),
+                            CustomPaint(
+                              size: const Size(285, 8),
+                              painter: BarPainterForExercise(),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                //참여중인 챌린지
+                Text(
+                  "참여중인 일정",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey.shade600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                FractionallySizedBox(
+                  widthFactor: 1,
+                  child: Container(
+                    constraints: const BoxConstraints(
+                      minHeight: 200,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 30,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "현재 참여중인 일정이 없습니다.",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey.shade400,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -94,28 +264,67 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class TimerPainter extends CustomPainter {
+class BarPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    // 타이머 틀
-    final center = Offset(size.width / 2, size.height / 2);
-    final radius = size.width / 2;
-    final bgPaint = Paint()
-      ..color = Colors.grey.shade300
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 15;
-    canvas.drawCircle(center, radius, bgPaint);
+    final bgLine = Paint()
+      ..color = Colors.grey.shade200
+      ..strokeWidth = 10
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke;
+    final fillLine = Paint()
+      ..color = Colors.deepPurple.shade400
+      ..strokeWidth = 10
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke;
+    canvas.drawLine(const Offset(0, 5), Offset(size.width, 5), bgLine);
+    canvas.drawLine(const Offset(0, 5), Offset(size.width - 50, 5), fillLine);
+  }
 
-    // 타이머 진행도
-    final progressPaint = Paint()
-      ..color = const Color(0xffff7979)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 15
-      ..strokeCap = StrokeCap.round;
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
 
-    final progressRect = Rect.fromCircle(center: center, radius: radius);
+class BarPainterForBook extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final bgLine = Paint()
+      ..color = Colors.grey.shade200
+      ..strokeWidth = 10
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke;
+    final fillLine = Paint()
+      ..color = Colors.red.shade400
+      ..strokeWidth = 10
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke;
+    canvas.drawLine(const Offset(0, 5), Offset(size.width, 5), bgLine);
+    canvas.drawLine(const Offset(0, 5), Offset(size.width - 80, 5), fillLine);
+  }
 
-    canvas.drawArc(progressRect, -1 / 2 * pi, 3 / 2 * pi, false, progressPaint);
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class BarPainterForExercise extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final bgLine = Paint()
+      ..color = Colors.grey.shade200
+      ..strokeWidth = 10
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke;
+    final fillLine = Paint()
+      ..color = Colors.green.shade400
+      ..strokeWidth = 10
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke;
+    canvas.drawLine(const Offset(0, 5), Offset(size.width, 5), bgLine);
+    canvas.drawLine(const Offset(0, 5), Offset(size.width - 20, 5), fillLine);
   }
 
   @override
